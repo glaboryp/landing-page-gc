97fd14528ea1d1ee44baaae4ef5da2ae5b733982
Gloria <glabory@gmail.com>
2024-04-12 17:50:30 +0200

    feat: Añadir a un archivo json los datos

---
+++ src/data/herramientas.js
@@ -0,0 +1,62 @@
+export const herramientas = [
+  {
+    id: 'internas',
+    title: 'Herramientas internas',
+    list: [
+      {
+        href: 'http://wikis.org/es/index.php/Harvest',
+        title: 'Harvest',
+        body: 'Gestor de expedientes para el trabajo de gobierno',
+        img: '/harvest.svg'
+      },
+      {
+        href: 'http://wikis.org/es/index.php/Orbis',
+        title: 'Orbis',
+        body: 'Base de datos institucional de la Prelatura',
+        img: '/orbis.png'
+      },
+      {
+        href: 'http://wikis.org/es/index.php/Outlook',
+        title: 'Outlook',
+        body: 'Mail personal y corporativo para comunicación externa',
+        img: '/outlook.png'
+      }
+    ]
+  },
+  {
+    id: 'externas',
+    title: 'Herramientas externas',
+    list: [
+      {
+        href: 'http://wikis.org/es/index.php/NextCloud',
+        title: 'NextCloud',
+        body: 'Nube privada para almacenamiento y colaboración en línea',
+        img: '/nextCloud.png'
+      },
+      {
+        href: '',
+        title: '',
+        body: '',
+        img: ''
+      }
+    ]
+  },
+  {
+    id: 'documentacion',
+    title: 'Documentación',
+    list: [
+      {
+        href: 'http://wikis.org/es/index.php',
+        title: 'Wikis GC',
+        body: 'Aquí encontrarás toda la documentación del proyecto Global Constellation y sus herramientas',
+        img: '/logo.png'
+      },
+      {
+        href: 'http://wikis.org/harvest_es/index.php',
+        title: 'Wiki Harvest',
+        body: 'Aquí encontrarás toda la documentación de Harvest',
+        img: '/harvest.svg'
+      }
+    ]
+  },
+]
