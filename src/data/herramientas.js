6e96e8b75de84286f29bec94696e05d226608fcd
Gloria <glabory@gmail.com>
2024-06-24 12:28:41 +0200

    config: Añadir links en inglés

--- src/data/herramientas.js
+++ src/data/herramientas.js
@@ -6,6 +6,7 @@ export const herramientas = [
     list: [
       {
         href: 'http://wikis.org/es/index.php/Harvest',
+        href_en: 'http://wikis.org/en/index.php/Harvest:_Studies_and_internal_mail',
         title: 'Harvest',
         cuerpo: 'Gestor de expedientes para el trabajo de la oficina',
         body: 'File manager for office work',
@@ -13,6 +14,7 @@ export const herramientas = [
       },
       {
         href: 'http://wikis.org/es/index.php/Orbis',
+        href_en: 'http://wikis.org/en/index.php/Orbis:_Personal_data',
         title: 'Orbis',
         cuerpo: 'Base de datos institucional',
         body: "Institutional database",
@@ -20,6 +22,7 @@ export const herramientas = [
       },
       {
         href: 'http://wikis.org/es/index.php/Outlook',
+        href_en: 'http://wikis.org/en/index.php/Outlook',
         title: 'Outlook',
         cuerpo: 'Mail personal y corporativo para comunicación',
         body: 'Personal and corporate mail for communication',
@@ -34,6 +37,7 @@ export const herramientas = [
     list: [
       {
         href: 'http://wikis.org/es/index.php/NextCloud',
+        href_en: 'http://wikis.org/en/index.php/NextCloud',
         title: 'NextCloud',
         cuerpo: 'Nube privada para almacenamiento y colaboración en línea',
         body: 'Private cloud for online storage and collaboration',
@@ -41,6 +45,7 @@ export const herramientas = [
       },
       {
         href: '',
+        href_en: '',
         title: 'Wekan',
         cuerpo: 'Herramienta de gestión de proyectos tipo Trello',
         body: 'Trello-like project management tool',
@@ -48,6 +53,7 @@ export const herramientas = [
       },
       {
         href: '',
+        href_en: '',
         title: 'Active Collab',
         cuerpo: 'Herramienta de gestión de proyectos y tareas',
         body: 'Project management tool that allows you to organize your work and task',
@@ -62,6 +68,7 @@ export const herramientas = [
     list: [
       {
         href: 'http://wikis.org/es/index.php',
+        href_en: 'http://wikis.org/en/index.php',
         title: 'Wikis GC',
         cuerpo: 'Aquí encontrarás toda la documentación del proyecto Global Constellation y sus herramientas',
         body: 'Here you will find all the Global Constellation project documentation and tools',
@@ -69,6 +76,7 @@ export const herramientas = [
       },
       {
         href: 'http://wikis.org/harvest_es/index.php',
+        href_en: 'http://wikis.org/harvest_en/index.php',
         title: 'Wiki Harvest',
         cuerpo: 'Aquí encontrarás toda la documentación de Harvest',
         body: 'Here you will find all Harvest documentation',
